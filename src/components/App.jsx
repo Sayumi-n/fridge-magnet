a5d53d616b604008cc0fe8576f72fa58acd72089
Sayumi <sayumi.nkmr.2019@gmail.com>
2020-03-26 17:39:47 -0700

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+// import words from '../api/words';
+// import StartBtn from './StartBtn';
+// import WordList from './WordList';
+// import CreatePhrase from './CreatePhrase';
+
+class App extends React.Component {
+  render() {
+    return <div className=""></div>;
+  }
+}
+
+export default App;
